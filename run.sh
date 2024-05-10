3a6a8446fc751a6689900d7cdc4c2d56d03a5ef9
Martin Janiczek <martin@janiczek.cz>
2024-05-10 19:44:22 +0200

    Init

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+MAIN_NAME="${1}"
+TMPDIR=$(mktemp -d -t "hvm-preprocess")
+cp -r ./* "${TMPDIR}/"
+cp -r ../_stdlib/* "${TMPDIR}/"
+cd "${TMPDIR}"
+for file in $(find . -name "*.hvml" | sed 's/^[.][/]//'); do
+  echo -e "#ifndef ${file^^}\n#define ${file^^}\n" \
+    | tr '.' '_' \
+    | cat - "${file}" >"${file}.tmp"
+  echo -e "\n#endif" >>"${file}.tmp"
+  mv "${file}.tmp" "${file}"
+done
+cd ..
+cpp -P -E "${TMPDIR}/${MAIN_NAME}" >"${TMPDIR}/_main.hvml"
+#clear && tput reset && echo -en "\033c\033[3J"
+#cat "${TMPDIR}/_main.hvml"
+hvml run -s -O all "${TMPDIR}/_main.hvml"
