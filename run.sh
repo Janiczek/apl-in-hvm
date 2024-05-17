529858fe8d6d687d7308c865f4f6965c01f84dac
Martin Janiczek <martin@janiczek.cz>
2024-05-17 10:34:30 +0200

    WIP bend rewrite

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 MAIN_NAME="${1}"
-TMPDIR=$(mktemp -d -t "hvm-preprocess")
+TMPDIR=$(mktemp -d -t hvm-preprocessXXXXX)
 cp -r ./* "${TMPDIR}/"
 cd "${TMPDIR}"
-for file in $(find . -name "*.hvml" | sed 's/^[.][/]//'); do
+for file in $(find . -name "*.bend" | sed 's/^[.][/]//'); do
   echo -e "#ifndef ${file^^}\n#define ${file^^}\n" \
     | tr '.' '_' \
     | cat - "${file}" >"${file}.tmp"
@@ -12,7 +12,7 @@ for file in $(find . -name "*.hvml" | sed 's/^[.][/]//'); do
   mv "${file}.tmp" "${file}"
 done
 cd ..
-cpp -P -E "${TMPDIR}/${MAIN_NAME}" >"${TMPDIR}/_main.hvml"
+cpp -P -E "${TMPDIR}/${MAIN_NAME}" >"${TMPDIR}/_main.bend"
 #clear && tput reset && echo -en "\033c\033[3J"
-#cat "${TMPDIR}/_main.hvml"
-hvml run -s -O all "${TMPDIR}/_main.hvml"
+#cat "${TMPDIR}/_main.bend"
+bend run-c -s "${TMPDIR}/_main.bend"
