2c6e0e8e24ae60fc7fb77a28b3aa4ee4b6d55ebc
Martin Janiczek <martin.janiczek@vendr.com>
2024-05-17 18:22:59 +0200

    Tweaky tweak

--- run.sh
+++ run.sh
@@ -15,55 +15,62 @@ for file in $(find . -name "*.bend" | sed 's/^[.][/]//'); do
 done
 popd >/dev/null
 
-## Test runner
-## For all Test.*.bend files in the current directory, make a Bend program that threads them together and run it
-## TODO make it parallel
 
-TEST_TMP_NAME="_test.bend"
-TEST_FILES=(Test.*.bend)
+## skip tests if TESTS=0
+if [ -z "${TESTS}" ]; then TESTS=1; fi
+if [ "${TESTS}" -ne 0 ]; then
 
-echo "Result/bind (Result/Ok val) fn = (fn val) " >>"${TMPDIR}/${TEST_TMP_NAME}"
-echo "Result/bind r               *  = r        " >>"${TMPDIR}/${TEST_TMP_NAME}"
-echo "_expect testName 0 = (Result/Err testName)" >>"${TMPDIR}/${TEST_TMP_NAME}"
-echo "_expect *        * = (Result/Ok 1)        " >>"${TMPDIR}/${TEST_TMP_NAME}"
+  ## Test runner
+  ## For all Test.*.bend files in the current directory, make a Bend program that threads them together and run it
+  ## TODO make it parallel
 
-for SINGLE_TEST_FILE in ${TEST_FILES[@]}; do
-  echo "#include \"${SINGLE_TEST_FILE}\"" >>"${TMPDIR}/${TEST_TMP_NAME}"
-done
+  TEST_TMP_NAME="_test.bend"
+  TEST_FILES=(Test.*.bend)
 
-for SINGLE_TEST_FILE in ${TEST_FILES[@]}; do
-  for TESTNAME in $(grep -o "^def test\([a-zA-Z0-9_./]\+\)" "${SINGLE_TEST_FILE}" | sed 's/^def //'); do
-    echo "def _${TESTNAME}(_):" >>"${TMPDIR}/${TEST_TMP_NAME}"
-    echo "  return _expect(\"${SINGLE_TEST_FILE}: ${TESTNAME}\",${TESTNAME}(*))" >>"${TMPDIR}/${TEST_TMP_NAME}"
+  echo "Result/bind (Result/Ok val) fn = (fn val) " >>"${TMPDIR}/${TEST_TMP_NAME}"
+  echo "Result/bind r               *  = r        " >>"${TMPDIR}/${TEST_TMP_NAME}"
+  echo "_expect testName 0 = (Result/Err testName)" >>"${TMPDIR}/${TEST_TMP_NAME}"
+  echo "_expect *        * = (Result/Ok 1)        " >>"${TMPDIR}/${TEST_TMP_NAME}"
+
+  for SINGLE_TEST_FILE in ${TEST_FILES[@]}; do
+    echo "#include \"${SINGLE_TEST_FILE}\"" >>"${TMPDIR}/${TEST_TMP_NAME}"
   done
-done
 
-echo "def main():"                  >>"${TMPDIR}/${TEST_TMP_NAME}"
-echo "  do Result:"                 >>"${TMPDIR}/${TEST_TMP_NAME}"
-for SINGLE_TEST_FILE in ${TEST_FILES[@]}; do
-  for TESTNAME in $(grep -o "^def test\([a-zA-Z0-9_./]\+\)" "${SINGLE_TEST_FILE}" | sed 's/^def //'); do
-    echo "    _ <- _$TESTNAME(*)" >>"${TMPDIR}/${TEST_TMP_NAME}"
+  for SINGLE_TEST_FILE in ${TEST_FILES[@]}; do
+    for TESTNAME in $(grep -o "^def test\([a-zA-Z0-9_./]\+\)" "${SINGLE_TEST_FILE}" | sed 's/^def //'); do
+      echo "def _${TESTNAME}(_):" >>"${TMPDIR}/${TEST_TMP_NAME}"
+      echo "  return _expect(\"${SINGLE_TEST_FILE}: ${TESTNAME}\",${TESTNAME}(*))" >>"${TMPDIR}/${TEST_TMP_NAME}"
+    done
   done
-done
-echo "    return \"All tests passed\"" >>"${TMPDIR}/${TEST_TMP_NAME}"
 
-## Run the test program
+  echo "def main():"                  >>"${TMPDIR}/${TEST_TMP_NAME}"
+  echo "  do Result:"                 >>"${TMPDIR}/${TEST_TMP_NAME}"
+  for SINGLE_TEST_FILE in ${TEST_FILES[@]}; do
+    for TESTNAME in $(grep -o "^def test\([a-zA-Z0-9_./]\+\)" "${SINGLE_TEST_FILE}" | sed 's/^def //'); do
+      echo "    _ <- _$TESTNAME(*)" >>"${TMPDIR}/${TEST_TMP_NAME}"
+    done
+  done
+  echo "    return \"All tests passed\"" >>"${TMPDIR}/${TEST_TMP_NAME}"
 
-cpp -P -E "${TMPDIR}/${TEST_TMP_NAME}" >"${TMPDIR}/pp.${TEST_TMP_NAME}"
-bend run-c -s "${TMPDIR}/pp.${TEST_TMP_NAME}" >"${TMPDIR}/_test_out.txt"
+  ## Run the test program
 
-## If the test out file contains "All tests passed", continue the script, otherwise exit
+  cpp -P -E "${TMPDIR}/${TEST_TMP_NAME}" >"${TMPDIR}/pp.${TEST_TMP_NAME}"
+  bend run-c -s "${TMPDIR}/pp.${TEST_TMP_NAME}" >"${TMPDIR}/_test_out.txt"
 
-if ! grep -q "All tests passed" "${TMPDIR}/_test_out.txt"; then
-  echo "Test failed!"
-  cat "${TMPDIR}/_test_out.txt" | head -n1 | sed 's/.*"\(.*\)".*/\1/'
-  echo
-  exit 1
+  ## If the test out file contains "All tests passed", continue the script, otherwise exit
+
+  if ! grep -q "All tests passed" "${TMPDIR}/_test_out.txt"; then
+    echo "Test failed!"
+    cat "${TMPDIR}/_test_out.txt" | head -n1 | sed 's/.*"\(.*\)".*/\1/'
+    echo
+    exit 1
+  fi
+
+  ## If successful, run the main program
+
+  echo -e "Tests passed!\n"
+
 fi
-
-## If successful, run the main program
-
-echo -e "Tests passed!\n"
 
 MAIN_NAME="${1}"
 MAIN_TMP_NAME="_main.bend"
